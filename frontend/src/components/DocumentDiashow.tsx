221ca5fd9657a7d66fba107875810e4fed4c9138
Lars Hagemann <hagemann.l@outlook.com>
2026-05-03 17:53:56 +0200

    feat: add swipe gestures to gallery navigation

--- frontend/src/components/DocumentDiashow.tsx
+++ frontend/src/components/DocumentDiashow.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useDocument } from "../hooks/useDocument";
 import { useDocumentPlugin } from "../hooks/useDocumentPlugin";
 import { useDocumentUrl } from "../hooks/useDocumentUrl";
+import { useSwipeable } from "react-swipeable";
 
 type Props = {
   documentId: string;
   nextDocument: () => void;
+  previousDocument: () => void;
   defaultTimeout?: number;
   mimeType?: string;
 };
@@ -13,6 +15,7 @@ type Props = {
 export const DocumentDiashow = ({
   documentId,
   nextDocument,
+  previousDocument,
   defaultTimeout = 3000,
   mimeType,
 }: Props) => {
@@ -27,16 +30,26 @@ export const DocumentDiashow = ({
 
   const objectUrl = isStream ? streamUrl : blobUrl;
 
+  const handlers = useSwipeable({
+    onSwipedLeft: () => nextDocument(),
+    onSwipedRight: () => previousDocument(),
+    trackMouse: true,
+    preventScrollOnSwipe: true,
+    trackTouch: true,
+  });
+
   if (!objectUrl) {
     return null;
   }
 
   return (
-    <plugin.Diashow
-      objectUrl={objectUrl}
-      defaultTimeout={defaultTimeout}
-      nextDocument={nextDocument}
-      React={React}
-    />
+    <div className="fixed z-10 bg-surface-1 w-screen h-screen left-0 top-0" {...handlers}>
+      <plugin.Diashow
+        objectUrl={objectUrl}
+        defaultTimeout={defaultTimeout}
+        nextDocument={nextDocument}
+        React={React}
+      />
+    </div>
   );
 };
